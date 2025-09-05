0c100a21b95f93e04bebed9222569202cb9901b8
Kvello <markus.kv1@gmail.com>
2025-09-05 16:16:37 +0200

    Removes CollectionProcessMananger. Not needed. Use global proc instead. This is more transparent

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,8 @@
 using namespace std;
 using namespace log4cpp;
 
+std::auto_ptr<PB5CollectionProcess> proc{ new PB5CollectionProcess() };
+
 void atExit(int signum)
 {
     static int caught(0);
@@ -23,24 +25,31 @@ void atExit(int signum)
     }
     printSigInfo(signum);
     printStackTrace();
-    DataCollectionProcessManager::getInstance().cleanup();
+    if (NULL != proc.get()) {
+        try {
+            delete proc.get();
+        } catch(exception& e) {
+            cout << e.what() << endl;
+        }
+    }
     exit(1);
 }
 
 int main (int argc, char *argv[])
 {
     int stat;
-
     setSignalHandler(atExit);
-    stat = DataCollectionProcessManager::getInstance()
-            .run(DataCollectionProcessManager::PB5, argc, argv);
-
-    /*  
-    if (0 == stat) {
-        cout << "STATUS : Data collection succcessful." << endl;
-    } else {
-        cout << "STATUS : Data collection failed." << endl;
+    try {
+        proc->init(argc,argv);
+        proc->run();
+    } 
+    catch (exception& e) {
+        cout << e.what() << endl;
+        stat = 1;
     }
-    */
+    catch (...) {
+        cout << "Exception caught while executing collection process." << endl;
+        stat = 1;
+    }
     return stat;
 }
